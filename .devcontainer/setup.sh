e1e55bcb6ab664c1c2b9adcdcb3ae92b47990fca
heerabete <125117306+heerabete@users.noreply.github.com>
2025-04-10 11:52:07 +0000

    Initial full repo push 🚀

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "🔥 Running OP Setup - Performance Mode ON..."
+
+apt update && apt install -y python3-venv tlp unzip jq && apt clean && rm -rf /var/lib/apt/lists/*
+
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+systemctl restart zramswap
+systemctl start docker
+systemctl enable tlp && systemctl start tlp
+
+npm install -g npm yarn pm2
+
+echo '* soft nofile 1048576' | tee -a /etc/security/limits.conf
+echo '* hard nofile 1048576' | tee -a /etc/security/limits.conf
+ulimit -n 1048576
+
+cd "/workspaces/C90"
+wget -q https://raw.githubusercontent.com/naksh-07/Automate/refs/heads/main/thorium.sh && chmod +x thorium.sh && ./thorium.sh
+
+curl -sSLO https://raw.githubusercontent.com/naksh-07/Automate/refs/heads/main/gaianet.sh && bash gaianet.sh
+curl -sSLO https://raw.githubusercontent.com/naksh-07/Automate/refs/heads/main/ognode.sh && bash ognode.sh
+
+echo "✅ All Done Bhai! Ultra OP Container READY 🚀"
